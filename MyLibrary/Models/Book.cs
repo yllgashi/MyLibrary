8421d80b81463ed02482d3041adb621f89faa10d
yllxgashi <yll@kutia.net>
2021-05-03 18:00:14 +0200

    create model classes

---
+++ MyLibrary/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyLibrary.Models
+{
+    public class Book
+    {
+        List<Author> Authors { get; set; }
+        List<Category> Categories { get; set; }
+    }
+}
---
+++ MyLibrary/Models/Offer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyLibrary.Models
+{
+    public class Offer
+    {
+    }
+}
