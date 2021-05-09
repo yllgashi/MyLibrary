a614c6bf514e8d56c883fd71187b2eb2ce45dd46
yllxgashi <yll@kutia.net>
2021-05-10 00:14:29 +0200

    fix edit bug

--- MyLibrary.Access/DatabaseConn.cs
+++ MyLibrary.Access/DatabaseConn.cs
@@ -10,6 +10,7 @@ namespace MyLibrary.DataAccess
     public class DatabaseConn
     {
         //public static string conString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString.ToString();
+
         //public static string conString = "Server=AS;Database=MyLibrary;Trusted_Connection=True;MultipleActiveResultSets=true";
         public static string conString = "Server=DESKTOP-A57SPHC;Database=MyLibrary;Trusted_Connection=True;MultipleActiveResultSets=true";
 
