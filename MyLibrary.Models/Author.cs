bc1fe2ac179cb31754b19cf9885f9f759932885d
yllgashi <yllxgashi@gmail.com>
2021-05-29 18:38:45 +0200

    add regex for author and book

--- MyLibrary.Models/Author.cs
+++ MyLibrary.Models/Author.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,8 +10,15 @@ namespace MyLibrary.Models
     public class Author
     {
         public int AuthorId { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        [RegularExpression("^[A-Z][a-zA-Z]*$")]
         public string FirstName { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        [RegularExpression("^[A-Z][a-zA-Z]*$")]
         public string LastName { get; set; }
+        [Required]
         public DateTime BirthDate { get; set; }
         public List<Book> Books { get; set; }
     }
--- MyLibrary.Models/Book.cs
+++ MyLibrary.Models/Book.cs
@@ -1,5 +1,6 @@
 ﻿ using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,12 +9,24 @@ namespace MyLibrary.Models
     public class Book
     {
         public int BookId { get; set; }
+        [Required]
+        [StringLength(50)]
         public string Title { get; set; }
+        [Required]
+        [StringLength(50)]
         public string Summary { get; set; }
+        [Required]
+        [MaxLength(4)]
+        [RegularExpression(@"^\d$")]
         public short PublishedYear { get; set; }
         public string Publisher { get; set; }
+        [Required]
+        [MaxLength(15)]
+        [MinLength(15)]
         public string ISBN { get; set; }
+        [RegularExpression(@"^\d$")]
         public int Pages { get; set; }
+        [RegularExpression(@"^\d$")]
         public double UnitPrice { get; set; }
         public List<Category> Categories { get; set; }
         public List<Author> Authors { get; set; }
